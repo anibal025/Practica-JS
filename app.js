0ba2f3b45cc877dba2842a75164abdeb94fee7c9
anibal025 <102227123+anibal025@users.noreply.github.com>
2022-05-14 14:31:27 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let calificacion=40;
+
+if (calificacion>=90)
+{
+    console.log('A');
+}
+else if (calificacion >=80)
+{
+    console.log('B');
+}
+else if (calificacion >=80)
+{
+    console.log('C');
+}
+else if (calificacion >=80)
+{
+    console.log('D');
+}
+else 
+console.log(F);
